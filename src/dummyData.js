0200efbe22c88f41eae3f6b52661eb963d8d67b3
Nimesh Solanki <nimesh3536@gmail.com>
2020-10-01 15:44:37 +0530

    add dummy data

---
+++ src/dummyData.js
@@ -0,0 +1,60 @@
+export const columns = [
+  {
+    id: '1',
+    label: 'Header_1',
+    numeric: false,
+    style: {
+      width: '10px',
+    },
+  },
+  {
+    id: '2',
+    label: 'Header_2',
+    numeric: false,
+    style: {
+      width: '5px',
+    },
+  },
+  {
+    id: '3',
+    label: 'Header_3',
+    numeric: false,
+    style: {
+      width: '50px',
+    },
+  },
+  {
+    id: '4',
+    label: 'Header_4',
+    numeric: false,
+    style: {
+      width: '45px',
+    },
+  },
+  {
+    id: '5',
+    label: 'Header_5',
+    numeric: false,
+    style: {
+      width: '85px',
+    },
+  },
+];
+
+export const rows = [
+  {
+    id: '11',
+    product: 'product_11',
+    price: 15.2,
+  },
+  {
+    id: '12',
+    product: 'product_12',
+    price: '$15.2',
+  },
+  {
+    id: '13',
+    product: 'product_13',
+    price: '15.99'
+  }
+];
