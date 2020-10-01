f8212be097ddc0019e20dbddb3adbd8de5f95372
Nimesh Solanki <nimesh3536@gmail.com>
2020-10-01 23:37:24 +0530

    add data to state and show it only after loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import DataTable from './Components/DataTable';
-import {rows, columns} from './dummyData';
-
+// import {rows, columns} from './dummyData';
+import { fetchAndConvertData } from './api';
 
 function App() {
+  const [columns, setColumns] = useState([]);
+  const [rows, setRows] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function getData() {
+      setIsLoading(true);
+      const { allColumns, allRows } = await fetchAndConvertData();
+      console.log('rows: ', allRows);
+      console.log('columnsL ', allColumns);
+      setRows(allRows);
+      setColumns(allColumns);
+      setIsLoading(false);
+    }
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <DataTable columns={columns} rows={rows} />
+    <div className='App'>
+      {isLoading && <div>Loading...</div>}
+      {!isLoading && <DataTable columns={columns} rows={rows} />}
     </div>
   );
 }
