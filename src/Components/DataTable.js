39763c73df4e6a30344e2a78a0b7bc4d8f366378
Nimesh Solanki <nimesh3536@gmail.com>
2020-10-03 08:23:51 +0530

    make use of css

--- src/Components/DataTable.js
+++ src/Components/DataTable.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import '../App.css';
 
 export default function DataTable({ columns, rows }) {
   const renderTableHeader = () => {
     return columns.map((col, i) => (
       <th key={i} style={col.style}>
-        {col.label}
+        {col.label.toUpperCase()}
       </th>
     ));
   };
 
   const renderTableBody = () => {
     return rows.map((row, i) => (
-      <tr key={i}>
+      <tr key={i} style={{ height: '160px' }}>
         <td>{row.albumId}</td>
         <td>{row.id}</td>
         <td>{row.title}</td>
@@ -26,8 +27,7 @@ export default function DataTable({ columns, rows }) {
   };
 
   return (
-    <div>
-      Hello
+    <div className='app-container'>
       <table>
         <thead>
           <tr>{renderTableHeader()}</tr>
